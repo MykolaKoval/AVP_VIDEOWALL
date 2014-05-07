a2da9c37fae4bc6757131ad7b6113dcc65476e21
Mykola Koval <mykola_koval@epam.com>
2014-05-07 18:32:54 +0300

    added window layout support for preset

--- vw-admin/src/main/java/com/atanor/vwserver/admin/mvp/view/edit/EditPresetView.java
+++ vw-admin/src/main/java/com/atanor/vwserver/admin/mvp/view/edit/EditPresetView.java
@@ -10,8 +10,11 @@ import com.atanor.vwserver.admin.mvp.event.ActivateGridEvent;
 import com.atanor.vwserver.admin.mvp.event.CleanGridActivationEvent;
 import com.atanor.vwserver.admin.ui.GridLabel;
 import com.atanor.vwserver.admin.ui.Utils;
+import com.atanor.vwserver.admin.ui.layout.LayoutWindow;
+import com.atanor.vwserver.admin.ui.layout.LayoutWindowChanged;
 import com.atanor.vwserver.common.rpc.dto.DisplayDto;
 import com.atanor.vwserver.common.rpc.dto.LayoutDto;
+import com.atanor.vwserver.common.rpc.dto.LayoutWindowDto;
 import com.atanor.vwserver.common.rpc.dto.PresetDto;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
@@ -19,16 +22,17 @@ import com.google.common.primitives.Ints;
 import com.google.web.bindery.event.shared.EventBus;
 import com.smartgwt.client.widgets.Canvas;
 
-public class EditPresetView extends AbstractDisplayView {
+public class EditPresetView extends AbstractDisplayView implements LayoutWindowChanged {
 
 	@Inject
 	private EventBus eventBus;
-	
+
 	private final Map<Integer, List<GridLabel>> rowGridPanels = Maps.newLinkedHashMap();
 	private final Map<Integer, List<GridLabel>> columnGridPanels = Maps.newLinkedHashMap();
-	
+
 	private Canvas display;
-	
+	private Canvas editor;
+
 	public EditPresetView() {
 		setHeight(Utils.getMainAreaHeight());
 		setWidth(Utils.getEditAreaWidth());
@@ -39,25 +43,42 @@ public class EditPresetView extends AbstractDisplayView {
 			removeChild(display);
 			display = null;
 		}
+		if (editor != null) {
+			removeChild(editor);
+			editor = null;
+		}
 	}
 
 	public void onNewPreset(final LayoutDto layoutDto, final DisplayDto displayDto) {
 		Preconditions.checkNotNull(displayDto, "Display dto can not be null");
-		
+
 		clean();
+
 		display = createAndAdjustDisplay(displayDto);
 		addChild(display);
-		
-		createDisplayGrid(displayDto);
+		createDisplayGrid(displayDto, display.getWidth(), display.getHeight());
+
+		editor = createEditor(display);
+		addChild(editor);
+		createLayoutWindows(layoutDto);
 	}
 
-	private void createDisplayGrid(final DisplayDto dto) {
-		final Long panelWidth = Math.round(Double.valueOf(display.getWidth()) / dto.getSegmentNumWidth());
-		final Long panelHeight = Math.round(Double.valueOf(display.getHeight()) / dto.getSegmentNumHeight());
+	private Canvas createEditor(final Canvas canvas) {
+		final Canvas editor = new Canvas();
+		editor.setWidth(canvas.getWidth());
+		editor.setHeight(canvas.getHeight());
+		editor.setLeft(canvas.getLeft());
+		editor.setTop(canvas.getTop());
+		return editor;
+	}
+
+	private void createDisplayGrid(final DisplayDto dto, final Integer displayWidth, final Integer displayHeight) {
+		final Long panelWidth = Math.round(Double.valueOf(displayWidth) / dto.getSegmentNumWidth());
+		final Long panelHeight = Math.round(Double.valueOf(displayHeight) / dto.getSegmentNumHeight());
 
 		createGridPanels(dto, panelWidth, panelHeight);
 	}
-	
+
 	private void createGridPanels(final DisplayDto dto, Long panelWidth, Long panelHeight) {
 		for (int row = 0, top = 0, left = 0; row < dto.getSegmentNumHeight(); row++) {
 			for (int col = 0; col < dto.getSegmentNumWidth(); col++) {
@@ -87,13 +108,42 @@ public class EditPresetView extends AbstractDisplayView {
 		display.addChild(panel);
 	}
 
+	private void createLayoutWindows(final LayoutDto layoutDto) {
+		if (layoutDto != null) {
+			for (final LayoutWindowDto windowDto : layoutDto.getWindows()) {
+				final LayoutWindowDto dto = toRealDimensions(windowDto);
+				dto.setName(windowDto.getName());				
+				createLayoutWindow(dto);
+			}
+		}
+	}
+
+	private void createLayoutWindow(final LayoutWindowDto windowDto) {
+		final LayoutWindow win = new LayoutWindow(windowDto, this);
+		editor.addChild(win);
+	}
+	
+	private LayoutWindowDto toRealDimensions(final LayoutWindowDto dto) {
+		final LayoutWindowDto result = new LayoutWindowDto();
+		result.setName(dto.getName());
+		result.setLeft(toRealValue(dto.getLeft(), display.getWidth()));
+		result.setTop(toRealValue(dto.getTop(), display.getHeight()));
+		result.setHeight(toRealValue(dto.getHeight(), display.getHeight()));
+		result.setWidth(toRealValue(dto.getWidth(), display.getWidth()));
+		return result;
+	}
+	
+	private static Integer toRealValue(final Integer percent, final Integer base) {
+		return base * percent / 100;
+	}
+	
 	@Override
 	protected Canvas createDisplay(final DisplayDto display) {
 		final Canvas canvas = new Canvas();
 		canvas.setBackgroundColor("grey");
 		return canvas;
 	}
-	
+
 	public void setPreset(final PresetDto preset) {
 	}
 
@@ -109,6 +159,12 @@ public class EditPresetView extends AbstractDisplayView {
 			columnGridPanels.put(col, new ArrayList<GridLabel>());
 		}
 		columnGridPanels.get(col).add(panel);
+	}
+
+	@Override
+	public void onLayoutWindowSelected(final LayoutWindow window) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
